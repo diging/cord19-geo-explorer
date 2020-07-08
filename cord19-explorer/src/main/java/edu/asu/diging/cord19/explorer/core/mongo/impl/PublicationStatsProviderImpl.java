6088c6b35fb6774de8e2ebc7d8a6d3d1f1bb592b
Julia Damerow <jdamerow@users.noreply.github.com>
2020-07-08 16:19:12 -0400

    Story/cord 23 (#14)
    * [CORD-23] fixed affiliation stats
    
    * [CORD-23] added affiliation matching stats
    
    * [CORD-23] make sure there is at least one document

--- cord19-explorer/src/main/java/edu/asu/diging/cord19/explorer/core/mongo/impl/PublicationStatsProviderImpl.java
+++ cord19-explorer/src/main/java/edu/asu/diging/cord19/explorer/core/mongo/impl/PublicationStatsProviderImpl.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.aggregation.CountOperation;
+import org.springframework.data.mongodb.core.aggregation.GroupOperation;
 import org.springframework.data.mongodb.core.aggregation.MatchOperation;
 import org.springframework.data.mongodb.core.aggregation.UnwindOperation;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -13,8 +14,8 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Controller;
 
 
-
 import edu.asu.diging.cord19.explorer.core.model.impl.PublicationImpl;
+import edu.asu.diging.cord19.explorer.core.model.impl.WikipediaSelectionStatus;
 import edu.asu.diging.cord19.explorer.core.mongo.PublicationStatsProvider;
 
 
@@ -36,6 +37,10 @@ public class PublicationStatsProviderImpl implements PublicationStatsProvider {
         
         Aggregation aggregation = Aggregation.newAggregation(unwind, count);
         AggregationResults<Document> doc = mongoTemplate.aggregate(aggregation, collection, Document.class); 
+        
+        if (doc.getMappedResults().isEmpty()) {
+            return 0;
+        }
         return doc.getMappedResults().get(0).getInteger("total");
     }
     
@@ -44,8 +49,20 @@ public class PublicationStatsProviderImpl implements PublicationStatsProvider {
      */
     @Override
     public long getPaperWithAuthorAffiliationCount() {
-        Query query = new Query(Criteria.where("metadata.authors.affiliation.locationCountry").ne(null));
-        return mongoTemplate.count(query, PublicationImpl.class);
+        String collection = mongoTemplate.getCollectionName(PublicationImpl.class);
+        
+        UnwindOperation unwind = Aggregation.unwind("metadata.authors");
+        MatchOperation match = Aggregation.match(Criteria.where("metadata.authors.affiliation.locationCountry").exists(true));
+        GroupOperation group = Aggregation.group("paperId");
+        CountOperation count  = Aggregation.count().as("total");
+        
+        Aggregation aggregation = Aggregation.newAggregation(unwind, match, group, count);
+        AggregationResults<Document> doc = mongoTemplate.aggregate(aggregation, collection, Document.class); 
+        
+        if (doc.getMappedResults().isEmpty()) {
+            return 0;
+        }
+        return doc.getMappedResults().get(0).getInteger("total");
     }
      
     /* (non-Javadoc)
@@ -55,11 +72,127 @@ public class PublicationStatsProviderImpl implements PublicationStatsProvider {
     public long getPaperWithoutAuthorAffiliationCount() {
         String collection = mongoTemplate.getCollectionName(PublicationImpl.class);
         
-        MatchOperation match = Aggregation.match(Criteria.where("metadata.authors").elemMatch(Criteria.where("affiliation.locationCountry").exists(false)));
+        MatchOperation match = Aggregation.match(Criteria.where("metadata.authors").not().elemMatch(Criteria.where("affiliation.locationCountry").exists(true)));
         CountOperation count  = Aggregation.count().as("total");
         
         Aggregation aggregation = Aggregation.newAggregation(match, count);
         AggregationResults<Document> doc = mongoTemplate.aggregate(aggregation, collection, Document.class); 
+        
+        if (doc.getMappedResults().isEmpty()) {
+            return 0;
+        }
+        return doc.getMappedResults().get(0).getInteger("total");
+    }
+    
+    @Override
+    public long getAuthorWithAffiliationCount() {
+        String collection = mongoTemplate.getCollectionName(PublicationImpl.class);
+        
+        UnwindOperation unwind = Aggregation.unwind("metadata.authors");
+        MatchOperation match = Aggregation.match(Criteria.where("metadata.authors.affiliation.locationCountry").exists(true));
+        CountOperation count  = Aggregation.count().as("total");
+        
+        Aggregation aggregation = Aggregation.newAggregation(unwind, match, count);
+        AggregationResults<Document> doc = mongoTemplate.aggregate(aggregation, collection, Document.class); 
+        
+        if (doc.getMappedResults().isEmpty()) {
+            return 0;
+        }
+        return doc.getMappedResults().get(0).getInteger("total");
+    }
+    
+    @Override
+    public long getAuthorWithAffiliationAndWikiarticleCount() {
+        String collection = mongoTemplate.getCollectionName(PublicationImpl.class);
+        
+        UnwindOperation unwind = Aggregation.unwind("metadata.authors");
+        MatchOperation match1 = Aggregation.match(Criteria.where("metadata.authors.affiliation.locationCountry").exists(true));
+        MatchOperation match2 = Aggregation.match(Criteria.where("metadata.authors.affiliation.selectedWikiarticle").exists(true));
+        CountOperation count  = Aggregation.count().as("total");
+        
+        Aggregation aggregation = Aggregation.newAggregation(unwind, match1, match2, count);
+        AggregationResults<Document> doc = mongoTemplate.aggregate(aggregation, collection, Document.class); 
+        
+        if (doc.getMappedResults().isEmpty()) {
+            return 0;
+        }
+        return doc.getMappedResults().get(0).getInteger("total");
+    }
+    
+    @Override
+    public long getPaperWithAtLeastOneAffiliationAndWikiarticleCount() {
+        String collection = mongoTemplate.getCollectionName(PublicationImpl.class);
+        
+        UnwindOperation unwind = Aggregation.unwind("metadata.authors");
+        MatchOperation match1 = Aggregation.match(Criteria.where("metadata.authors.affiliation.locationCountry").exists(true));
+        MatchOperation match2 = Aggregation.match(Criteria.where("metadata.authors.affiliation.selectedWikiarticle").exists(true));
+        GroupOperation group = Aggregation.group("paperId");
+        CountOperation count  = Aggregation.count().as("total");
+        
+        Aggregation aggregation = Aggregation.newAggregation(unwind, match1, match2, group, count);
+        AggregationResults<Document> doc = mongoTemplate.aggregate(aggregation, collection, Document.class); 
+        
+        if (doc.getMappedResults().isEmpty()) {
+            return 0;
+        }
+        return doc.getMappedResults().get(0).getInteger("total");
+    }
+    
+    @Override
+    public long getAuthorsWithAffiliationAndIncorrectWikiarticleCount() {
+        String collection = mongoTemplate.getCollectionName(PublicationImpl.class);
+        
+        UnwindOperation unwind = Aggregation.unwind("metadata.authors");
+        MatchOperation match1 = Aggregation.match(Criteria.where("metadata.authors.affiliation.locationCountry").exists(true));
+        MatchOperation match2 = Aggregation.match(Criteria.where("metadata.authors.affiliation.selectedWikiarticle").exists(true));
+        MatchOperation match3 = Aggregation.match(Criteria.where("metadata.authors.affiliation.selectionStatus").is(WikipediaSelectionStatus.INCORRECT));
+        CountOperation count  = Aggregation.count().as("total");
+        
+        Aggregation aggregation = Aggregation.newAggregation(unwind, match1, match2, match3, count);
+        AggregationResults<Document> doc = mongoTemplate.aggregate(aggregation, collection, Document.class); 
+        
+        if (doc.getMappedResults().isEmpty()) {
+            return 0;
+        }
+        return doc.getMappedResults().get(0).getInteger("total");
+    }
+    
+    @Override
+    public long getPapersWithAffiliationAndIncorrectWikiarticleCount() {
+        String collection = mongoTemplate.getCollectionName(PublicationImpl.class);
+        
+        UnwindOperation unwind = Aggregation.unwind("metadata.authors");
+        MatchOperation match1 = Aggregation.match(Criteria.where("metadata.authors.affiliation.locationCountry").exists(true));
+        MatchOperation match2 = Aggregation.match(Criteria.where("metadata.authors.affiliation.selectedWikiarticle").exists(true));
+        MatchOperation match3 = Aggregation.match(Criteria.where("metadata.authors.affiliation.selectionStatus").is(WikipediaSelectionStatus.INCORRECT));
+        GroupOperation group = Aggregation.group("paperId");
+        CountOperation count  = Aggregation.count().as("total");
+        
+        Aggregation aggregation = Aggregation.newAggregation(unwind, match1, match2, match3, group, count);
+        AggregationResults<Document> doc = mongoTemplate.aggregate(aggregation, collection, Document.class); 
+        
+        if (doc.getMappedResults().isEmpty()) {
+            return 0;
+        }
+        return doc.getMappedResults().get(0).getInteger("total");
+    }
+    
+    @Override
+    public long getAuthorsWithAffiliationAndCorrectRegionWikiarticleCount() {
+        String collection = mongoTemplate.getCollectionName(PublicationImpl.class);
+        
+        UnwindOperation unwind = Aggregation.unwind("metadata.authors");
+        MatchOperation match1 = Aggregation.match(Criteria.where("metadata.authors.affiliation.locationCountry").exists(true));
+        MatchOperation match2 = Aggregation.match(Criteria.where("metadata.authors.affiliation.selectedWikiarticle").exists(true));
+        MatchOperation match3 = Aggregation.match(Criteria.where("metadata.authors.affiliation.selectionStatus").is(WikipediaSelectionStatus.CORRECT_REGION));
+        CountOperation count  = Aggregation.count().as("total");
+        
+        Aggregation aggregation = Aggregation.newAggregation(unwind, match1, match2, match3, count);
+        AggregationResults<Document> doc = mongoTemplate.aggregate(aggregation, collection, Document.class); 
+        
+        if (doc.getMappedResults().isEmpty()) {
+            return 0;
+        }
         return doc.getMappedResults().get(0).getInteger("total");
     }
 }

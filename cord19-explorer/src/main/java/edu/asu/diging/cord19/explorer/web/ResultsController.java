441a1aa6a667d4034055dd84ce5c18cfb4d1da44
Aakash Kumar <aakash994@gmail.com>
2020-07-10 01:20:41 -0700

    [cord-12]search box on home

---
+++ cord19-explorer/src/main/java/edu/asu/diging/cord19/explorer/web/ResultsController.java
@@ -0,0 +1,15 @@
+package edu.asu.diging.cord19.explorer.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ResultsController {
+
+	@RequestMapping(value = "/result")
+    public String home(Model model) {
+        return "results";
+    }
+
+}

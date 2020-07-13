33ab585b9081026c990467edbab31e84cb54b8f1
Aakash Kumar <aakash994@gmail.com>
2020-07-13 03:10:38 -0700

    [cord-12]indentation

--- cord19-explorer/src/main/java/edu/asu/diging/cord19/explorer/web/ResultsController.java
+++ cord19-explorer/src/main/java/edu/asu/diging/cord19/explorer/web/ResultsController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ResultsController {
 
-	@RequestMapping(value = "/result")
+    @RequestMapping(value = "/result")
     public String home(Model model) {
         return "results";
     }

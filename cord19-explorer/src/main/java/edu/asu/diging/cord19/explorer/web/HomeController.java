e5774e261a90441fbb663f48d42440ad417a15bd
adhvikmadhav <adhvikmadhav97@gmail.com>
2020-08-07 12:51:07 -0700

    [CORD-28] Fixed login task flow

--- cord19-explorer/src/main/java/edu/asu/diging/cord19/explorer/web/HomeController.java
+++ cord19-explorer/src/main/java/edu/asu/diging/cord19/explorer/web/HomeController.java
@@ -21,20 +21,30 @@ public class HomeController {
 
     @RequestMapping(value = "/")
     public String home(Model model) {
-        model.addAttribute("countries", pubDao.getCountries());
-        model.addAttribute("cleanedAffiliations", pubDao.getDistinctAffiliations());
+        //model.addAttribute("countries", pubDao.getCountries());
+        //model.addAttribute("cleanedAffiliations", pubDao.getDistinctAffiliations());
         model.addAttribute("authorCount", provider.getAuthorCount());
         model.addAttribute("paperWithAffCount", provider.getPaperWithAuthorAffiliationCount());
         model.addAttribute("years", pubDao.getYears());
         model.addAttribute("totalPublications", repo.count());
-        model.addAttribute("textCountries", pubDao.getCountriesInText());
-        model.addAttribute("publicationCount", pubDao.getPublicationCount());
-        model.addAttribute("affiliationCount", pubDao.getAffiliationCount());
-        model.addAttribute("cleanedAffiliationCount", pubDao.getDistinctAffiliationCount());
-        model.addAttribute("yearCount", pubDao.getYearCount());
-        model.addAttribute("journals", pubDao.getJournals());
+       // model.addAttribute("textCountries", pubDao.getCountriesInText());
+        //model.addAttribute("publicationCount", pubDao.getPublicationCount());
+        //model.addAttribute("affiliationCount", pubDao.getAffiliationCount());
+        //model.addAttribute("cleanedAffiliationCount", pubDao.getDistinctAffiliationCount());
+       // model.addAttribute("yearCount", pubDao.getYearCount());
+        //model.addAttribute("journals", pubDao.getJournals());
         model.addAttribute("journalCount", pubDao.getJournalCount());
-        model.addAttribute("textCountriesCount", pubDao.getCountriesInTextCount());
+        //model.addAttribute("textCountriesCount", pubDao.getCountriesInTextCount());
         return "home";
+    }
+    
+    @RequestMapping(value ="/login")
+    public String login() {
+      return "login";
+    }
+    @RequestMapping(value ="/loginFailed")
+    public String loginFailed(Model model) {
+      model.addAttribute("loginError", true);
+      return "login";
     }
 }

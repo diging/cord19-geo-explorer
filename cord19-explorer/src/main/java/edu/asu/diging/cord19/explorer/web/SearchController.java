fa55265db4242d7e8d8f9cb81dfd11af2d1bb91e
Aakash Kumar <aakash994@gmail.com>
2020-09-28 22:50:17 -0700

    [CORD-29] path variable fix

--- cord19-explorer/src/main/java/edu/asu/diging/cord19/explorer/web/SearchController.java
+++ cord19-explorer/src/main/java/edu/asu/diging/cord19/explorer/web/SearchController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,11 +23,11 @@ public class SearchController {
     @Autowired
     private Map<SearchType, SearchProvider> searchProviderRegistry;
 
-    @RequestMapping(value = "/search")
-    public String search(@RequestParam("searchType") String searchType, @RequestParam("search") String query,
+    @RequestMapping(value = "/{searchType}/search")
+    public String search(@PathVariable("searchType") SearchType searchType, @RequestParam("search") String query,
             Model model, @PageableDefault(size = 20) Pageable pageable) {
 
-        if (searchType.equals(SearchType.AFFILIATIONS.toString())) {
+        if (searchType.equals(SearchType.AFFILIATIONS)) {
             @SuppressWarnings("unchecked")
             List<AffiliationPaperAggregationOutput> matchedAffiliationsList = (List<AffiliationPaperAggregationOutput>) searchProviderRegistry
                     .get(SearchType.AFFILIATIONS)
